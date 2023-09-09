e7570c7b3482739f87876d18c0dc6587a2a202f8
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-09 07:00:28 +0530

    Project from autocude

---
+++ AulaCude/src/test/java/com/aulacude/AulaCudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulacude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaCudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
