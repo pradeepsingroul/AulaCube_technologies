df7ba4e499f4be3e958a14f0cd387292b3680d1b
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-09 06:59:48 +0530

    Main

---
+++ AulaCude/src/main/java/com/aulacude/AulaCudeApplication.java
@@ -0,0 +1,13 @@
+package com.aulacude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaCudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaCudeApplication.class, args);
+	}
+
+}
