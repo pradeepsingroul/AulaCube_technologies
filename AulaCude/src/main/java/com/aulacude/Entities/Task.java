2023bcc6815b163a145eacba6edd143fdde6eb53
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-09 06:58:22 +0530

    entities

---
+++ AulaCude/src/main/java/com/aulacude/Entities/Task.java
@@ -0,0 +1,67 @@
+package com.aulacude.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+	
+	@Id
+	private Integer id;
+	
+	private String title;
+	
+	private String description;
+	
+	private String status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Task(Integer id, String title, String description, String status) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.status = status;
+	}
+
+	public Task() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+}
---
+++ AulaCude/src/main/java/com/aulacude/Entities/User.java
@@ -0,0 +1,103 @@
+package com.aulacude.Entities;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+
+@Entity
+public class User {
+	
+	
+	@Id
+	private Integer id;
+	
+	private String userName;
+	
+	@Email(message = "email should be in proper format")
+	private String email;
+	
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private String password;
+	
+	
+	private String address;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public User(Integer id, String userName, @Email(message = "email should be in proper format") String email,
+			String password, String address) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+	}
+
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+	
+
+}
