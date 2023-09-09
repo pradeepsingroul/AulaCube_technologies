9f4a12342a5c414bcc7e9b5906d15d0775493cc6
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-09 06:59:21 +0530

    Security config

---
+++ AulaCude/src/main/java/com/aulacude/Configuration/AppConfig.java
@@ -0,0 +1,41 @@
+package com.aulacude.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+
+	
+	//spring security - implementing security to the methods
+	@Bean
+	public SecurityFilterChain springSecurityConfiguration(HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests().requestMatchers(HttpMethod.POST,"/users")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.csrf()
+		.disable()
+		.formLogin()
+		.and()
+		.httpBasic();
+		
+		return http.build();
+		
+	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+	
+	
+}
